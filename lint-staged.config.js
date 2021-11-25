4412a108b8033477da64ea6c38609a43e8288a7b
kukuh <kukuh.baiquni@gmail.com>
2021-11-25 15:17:00 +0700

    remove prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,12 +3,5 @@ module.exports = {
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn run fix ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
-  ],
-
-  // Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) =>
-    `yarn prettier --write ${filenames.join(' ')}`,
+  '**/*.(ts|tsx|js)': (filenames) => [`yarn run fix ${filenames.join(' ')}`],
 }
