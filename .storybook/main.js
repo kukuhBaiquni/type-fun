62399da9706cc776587737ac4120d3a3c7f0995d
kukuh <kukuh.baiquni@gmail.com>
2021-11-30 18:13:07 +0700

    fix header story

--- .storybook/main.js
+++ .storybook/main.js
@@ -29,14 +29,6 @@ module.exports = {
     //   use: [ 'postcss-loader', 'sass-loader', 'style-loader', 'css-loader' ],
     //   include: path.resolve(__dirname, '../'),
     // })
-    config.module.rules.push({
-      test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/,
-      loader: require.resolve('file-loader'), // file-loader
-      options: {
-        name: 'static/media/[name].[hash:8].[ext]',
-        esModule: false,
-      },
-    })
     config.resolve.modules.push(process.cwd() + "/src");
     return { ...nextConfig.webpack, ...config }
   },
