640f92d7cf035933ef1ee6d5089f176247ff3a4b
kukuh <kukuh.baiquni@gmail.com>
2021-12-02 16:15:10 +0700

    draft article details

---
+++ src/constant/articles.ts
@@ -0,0 +1,24 @@
+const articles = [{
+  title: 'Elsword World',
+  img: 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Farticle%2Felsword-world.jpg?alt=media&token=b1020221-4b36-4fe1-a937-314586d8f6a3',
+  path: '/related-articles/elsword-world',
+}, {
+  title: 'Non Playable Character (NPC)',
+  img: 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Farticle%2Fnpc.jpg?alt=media&token=e82c9e7c-18c3-443e-85e8-4fddc8622f82',
+  path: '/related-articles/npc',
+}, {
+  title: 'Item Mall & Ice Burner',
+  img: 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Farticle%2Fitem-mall.jpg?alt=media&token=a5ab4a67-64ac-43fb-a1d3-a0d74bd6df02',
+  path: '/related-articles/item-mall',
+}, {
+  title: 'Story',
+  img: 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Farticle%2Fstory.jpg?alt=media&token=73c48d9a-8ce5-4885-902e-131a5b647c18',
+  path: '/related-articles/story',
+}]
+
+export const elswordWorldMap = 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Farticle%2Fassets%2Felsword-world.png?alt=media&token=855203e3-a513-4fba-a030-956bbd9b2b91'
+export const demonRealmsMap = 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Farticle%2Fassets%2Fdemon-realms.png?alt=media&token=76ff4db7-a43d-466b-a07d-69f832be9ef4'
+export const lurensiaIsland = 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Farticle%2Fassets%2Flurensia.png?alt=media&token=0fa87cd9-1d72-49f2-b1ff-cc3d107c92ce'
+export const fluoneIsland = 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Farticle%2Fassets%2Ffluone.png?alt=media&token=b57d3342-0277-4b6a-8ccb-16773844eb65'
+
+export default articles
