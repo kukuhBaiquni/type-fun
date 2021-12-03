24b85a8405f0d1fdfd3a50e9f3a33038349d7cf8
kukuh <kukuh.baiquni@gmail.com>
2021-12-03 13:08:41 +0700

    draft elsword world

--- src/pages/related-articles/elsword-world.tsx
+++ src/pages/related-articles/elsword-world.tsx
@@ -9,6 +9,12 @@ import articles, {
 import loader from 'components/common/image-loader'
 
 const info = articles[0]
+const assetList = [
+  { img: elswordWorldMap, name: 'Elsword World' },
+  { img: demonRealmsMap, name: 'Demon Realms' },
+  { img: lurensiaIsland, name: 'Lurensia Island' },
+  { img: fluoneIsland, name: 'Fluone Island' },
+]
 
 const ElswordWorld = () => (
   <React.Fragment>
@@ -22,13 +28,14 @@ const ElswordWorld = () => (
       <section>
         <h3 className='text-2xl font-semibold font-titillium text-sky-500'>Elsword World</h3>
         <div className='mt-2 border-general border-l-0 border-b-0 border-r-0 pt-4'>
-          <div className='relative mb-2 h-[250px] md:h-[550px] bg-pink-500'>
+          <div className='relative mb-2 h-[250px] md:h-[550px]'>
             <Image
               alt='Elsword World'
               blurDataURL={loader('100%', '100%')}
               className='object-cover'
               layout='fill'
               placeholder='blur'
+              priority
               src={info.img}
             />
           </div>
@@ -43,52 +50,24 @@ const ElswordWorld = () => (
           </p>
         </div>
       </section>
-      <section className='grid grid-cols-4 gap-x-3'>
-        <div className='relative h-[200px] flex flex-col'>
-          <Image
-            alt='Elsword World Map'
-            blurDataURL={loader('100%', '100%')}
-            className='object-contain'
-            layout='fill'
-            placeholder='blur'
-            src={elswordWorldMap}
-          />
-        </div>
-        <div className='relative h-[250px]'>
-          <Image
-            alt='Demon Realms Map'
-            blurDataURL={loader('100%', '100%')}
-            className='object-contain'
-            layout='fill'
-            placeholder='blur'
-            src={demonRealmsMap}
-          />
-        </div>
-
-        <div className='relative h-[250px]'>
-          <Image
-            alt='Fluone Island'
-            blurDataURL={loader('100%', '100%')}
-            className='object-contain'
-            layout='fill'
-            placeholder='blur'
-            src={fluoneIsland}
-          />
-        </div>
+      <section className='grid grid-cols-4 gap-2'>
+        {assetList.map((item, index) => (
+          <div className='relative h-[200px]' key={index}>
+            <Image
+              alt={item.name}
+              blurDataURL={loader('100%', '100%')}
+              className='object-contain'
+              height={200}
+              placeholder='blur'
+              src={item.img}
+              width={300}
+            />
+            <p className='text-sm font-titillium'>{item.name}</p>
+          </div>
+        ))}
       </section>
       <section>
         <h3 className='text-xl font-titillium text-sky-500'>Lurensia</h3>
-        <div className='relative h-[250px] flex justify-start'>
-          <Image
-            alt='Lurensia Island'
-            blurDataURL={loader('100%', '100%')}
-            className='object-contain'
-            layout='fill'
-            placeholder='blur'
-            src={lurensiaIsland}
-          />
-          <p>Lurensia Island</p>
-        </div>
         <div className='mt-2 border-general border-l-0 border-b-0 border-r-0 pt-2'>
           <p>
             The Elrios supercontinent has been divided into two continents, Lurensia being the western one.
