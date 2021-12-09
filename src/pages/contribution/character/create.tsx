9f2771d24e801727742263a4ffce8b9ebe44f1d6
kukuh <kukuh.baiquni@gmail.com>
2021-12-09 18:59:01 +0700

    Draft story

--- src/pages/contribution/character/create.tsx
+++ src/pages/contribution/character/create.tsx
@@ -1,9 +1,21 @@
 import { ReactElement } from 'react'
 import Layout from 'components/layouts/default-layout'
+import InputText from 'components/forms/input-text'
+import { useForm } from 'react-hook-form'
 
-const Create = () => (
-  <div>Character Information</div>
-)
+const Create = () => {
+  const { register } = useForm()
+  return (
+    <div>
+      <InputText
+        label='Name'
+        name='name'
+        placeholder='Name'
+        register={register}
+      />
+    </div>
+  )
+}
 
 Create.getLayout = (page: ReactElement) => (
   <Layout>
